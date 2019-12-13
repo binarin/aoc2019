5499e9a37586e869528f8ee5ce4d3affbd2331e7
Alexey Lebedeff <binarin@binarin.ru>
2019-12-13 08:49:32 +0100

    Day 13 - part 1

--- 02-1.hs
+++ 02-1.hs
@@ -9,9 +9,13 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Main where
 
+import Data.List.Extra (chunksOf)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C8
 import Graphics.Gloss
 import qualified Data.HashTable.IO as H
-import Control.Lens
+import Control.Lens hiding (Empty)
 import Control.Lens.TH
 import Control.Monad.Extra (whileM)
 import Data.IORef
@@ -309,7 +313,6 @@ runDay11 = do
         runIntcode prog
         allCells <- H.toList hull
         let cells = view _1 <$> filter (view _2) allCells
-            square = Polygon [(0, 0), (0, 1), (1, 1), (1, 0)]
             panel (x, y) = Color white (Translate (fromIntegral x) (fromIntegral $ - y) square)
             allPanels = Pictures $ panel <$> cells
 
@@ -323,10 +326,40 @@ runIntcode :: Program -> IO ()
 runIntcode p = void $ execStateT (runApp run) p
 
 main :: IO ()
-main = runDay11
+main = runDay13
 
-runDay8 :: IO ()
-runDay8 = do
+
+data Tile = Empty | Wall | Block | Padddle | Ball deriving (Eq, Show, Enum)
+
+
+blockSize :: Int
+blockSize = 8
+
+tileAt :: Int -> Int -> Picture -> Picture
+tileAt x y pic = Translate (fromIntegral $ x * blockSize) (negate $ fromIntegral $ y * blockSize) $ Scale (fromIntegral blockSize) (fromIntegral blockSize) pic
+
+square :: Picture
+square = Polygon [(0, 0), (0, 1), (1, 1), (1, 0)]
+
+tileToPicture :: Tile -> Picture
+tileToPicture Empty = Blank
+tileToPicture Wall = Color red square
+tileToPicture Block = Color (makeColorI 127 127 127 255) square
+tileToPicture Padddle = Color blue square
+tileToPicture Ball = Color orange $ Translate 0.5 0.5 $ Circle 0.5
+
+
+drawArcade :: [MachineWord] -> Picture
+drawArcade output = Pictures $ go (reverse output)
+  where
+    go :: [MachineWord] -> [Picture]
+    go (x:y:tile:rest) = tileAt (fromIntegral x) (fromIntegral y) (tileToPicture $ toEnum $ fromIntegral tile) : go rest
+    go _ = []
+
+
+
+runDay13 :: IO ()
+runDay13 = do
   let scratch = [ 4, 50,  3, 50,  4, 50, 99, 51, 99,  0,
                   5,  5,  0,  0,  0,  0,  0,  0,  0,  0,
                   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
@@ -335,6 +368,35 @@ runDay8 = do
                  42, 13,  0,  0,  0,  0,  0,  0,  0,  0
                 ]
 
+  inputAction <- mkListInput []
+  (outputRef, outputAction) <- mkListOutput
+  let haltAction = putStrLn "halted"
+      opcodes = mkOpcodesTable (pureOpcodes ++ mkIoOpcodes haltAction inputAction outputAction)
+  d13 <- readProgram "13.txt"
+  p <- makeRunnable d13 opcodes
+  runIntcode p
+  output <- readIORef outputRef
+  let chunked :: [[MachineWord]] = chunksOf 3 $ reverse output
+
+  visibleSet :: H.LinearHashTable (Int, Int) Tile <- H.new
+  forM_ chunked $ \(x:y:b:_) -> do
+    H.insert visibleSet (fromIntegral x, fromIntegral y) (toEnum $ fromIntegral b)
+
+  visible <- H.toList visibleSet
+  let blocks = [ True | (_, Block) <- visible ]
+  print $ length blocks
+
+  display (InWindow "Nice Window" (200, 200) (10, 10)) black (drawArcade output)
+  pure ()
+
+readProgram :: FilePath -> IO [MachineWord]
+readProgram path = do
+  content <- B.readFile path
+  pure $ read . C8.unpack <$> C8.split ',' content
+
+
+runDay8 :: IO ()
+runDay8 = do
   let selfCopy = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
 
   inputAction <- mkListInput [2]
@@ -356,7 +418,6 @@ d8 = [1102,34463338,34463338,63,1007,63,34463338,63,1005,63,53,1101,0,3,1000,109
 
 d11 :: [MachineWord]
 d11 = [3,8,1005,8,330,1106,0,11,0,0,0,104,1,104,0,3,8,102,-1,8,10,101,1,10,10,4,10,1008,8,0,10,4,10,102,1,8,29,2,9,4,10,1006,0,10,1,1103,17,10,3,8,102,-1,8,10,101,1,10,10,4,10,108,0,8,10,4,10,101,0,8,61,1006,0,21,1006,0,51,3,8,1002,8,-1,10,101,1,10,10,4,10,108,1,8,10,4,10,1001,8,0,89,1,102,19,10,1,1107,17,10,1006,0,18,3,8,1002,8,-1,10,1001,10,1,10,4,10,1008,8,1,10,4,10,1001,8,0,123,1,9,2,10,2,1105,10,10,2,103,9,10,2,1105,15,10,3,8,102,-1,8,10,1001,10,1,10,4,10,1008,8,0,10,4,10,102,1,8,161,3,8,102,-1,8,10,101,1,10,10,4,10,108,1,8,10,4,10,101,0,8,182,3,8,1002,8,-1,10,101,1,10,10,4,10,1008,8,0,10,4,10,101,0,8,205,2,1102,6,10,1006,0,38,2,1007,20,10,2,1105,17,10,3,8,102,-1,8,10,1001,10,1,10,4,10,108,1,8,10,4,10,1001,8,0,241,3,8,102,-1,8,10,101,1,10,10,4,10,108,1,8,10,4,10,101,0,8,263,1006,0,93,2,5,2,10,2,6,7,10,3,8,102,-1,8,10,101,1,10,10,4,10,108,0,8,10,4,10,1001,8,0,296,1006,0,81,1006,0,68,1006,0,76,2,4,4,10,101,1,9,9,1007,9,1010,10,1005,10,15,99,109,652,104,0,104,1,21102,825594262284,1,1,21102,347,1,0,1105,1,451,21101,0,932855939852,1,21101,358,0,0,1106,0,451,3,10,104,0,104,1,3,10,104,0,104,0,3,10,104,0,104,1,3,10,104,0,104,1,3,10,104,0,104,0,3,10,104,0,104,1,21102,1,235152649255,1,21101,405,0,0,1105,1,451,21102,235350879235,1,1,21102,416,1,0,1106,0,451,3,10,104,0,104,0,3,10,104,0,104,0,21102,988757512972,1,1,21101,439,0,0,1106,0,451,21102,1,988669698828,1,21101,0,450,0,1106,0,451,99,109,2,22101,0,-1,1,21102,40,1,2,21102,1,482,3,21102,472,1,0,1106,0,515,109,-2,2105,1,0,0,1,0,0,1,109,2,3,10,204,-1,1001,477,478,493,4,0,1001,477,1,477,108,4,477,10,1006,10,509,1101,0,0,477,109,-2,2106,0,0,0,109,4,1202,-1,1,514,1207,-3,0,10,1006,10,532,21102,1,0,-3,21202,-3,1,1,21202,-2,1,2,21102,1,1,3,21102,1,551,0,1106,0,556,109,-4,2105,1,0,109,5,1207,-3,1,10,1006,10,579,2207,-4,-2,10,1006,10,579,22101,0,-4,-4,1105,1,647,21201,-4,0,1,21201,-3,-1,2,21202,-2,2,3,21102,598,1,0,1105,1,556,21202,1,1,-4,21101,0,1,-1,2207,-4,-2,10,1006,10,617,21102,1,0,-1,22202,-2,-1,-2,2107,0,-3,10,1006,10,639,21202,-1,1,1,21102,1,639,0,105,1,514,21202,-2,-1,-2,22201,-4,-2,-4,109,-5,2105,1,0]
-
 
 
 -- program :: [Integer]
