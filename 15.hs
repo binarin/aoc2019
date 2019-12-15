233bc0871ac7988571d263a3c00bc128541aca06
Alexey Lebedeff <binarin@binarin.ru>
2019-12-15 19:20:31 +0100

    Day 15 - WIP

--- 15.hs
+++ 15.hs
@@ -1,32 +1,36 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 module Main where
 
+import Control.Monad.IO.Class
 import qualified Data.PQueue.Min as Q
 import Control.Lens hiding (Empty)
 import qualified Data.Map as M
 import Control.Lens.TH
-import Data.IORef (newIORef)
+import Data.IORef (newIORef, readIORef, writeIORef)
 import qualified Data.Set as S
+import Graphics.Gloss hiding (Point)
 
 import IntCode
 
 type Coord = Int
 type Point = (Int, Int)
 
-data Cell = Empty | Wall | OxygenTank | Unknown deriving (Show, Eq)
+data Cell = Wall | Empty | OxygenTank | Unknown deriving (Show, Eq, Enum)
 data Direction = North | East | South | West deriving (Show, Eq)
 
 type Map = M.Map Point Cell
 
 data ExplorationStatus = Moving [Direction] | Exploring deriving (Show)
 
-data PlotState = PlotState { _plotStatePositionL :: Point
-                           , _plotStateMapL :: Map
-                           , _plotStateQueueL :: S.Set Point
+data StackOp = Push Direction Point | Pop Direction deriving (Show)
+
+data PlotState = PlotState { _plotStateMapL :: Map
+                           , _plotStateQueueL :: [StackOp]
                            } deriving (Show)
 
 makeFields ''PlotState
@@ -36,28 +40,6 @@ getPlotCell plot (x, y) =
   case M.lookup (x, y) (plot^.mapL) of
     Nothing -> Unknown
     Just cell -> cell
-
-
-plotOnArrival :: PlotState -> PlotState
-plotOnArrival old = new
-  where
-    thisC = old^.positionL
-    upC = thisC & _2 %~ pred
-    downC = thisC & _2 %~ succ
-    leftC = thisC & _1 %~ pred
-    rightC = thisC & _1 %~ succ
-    unknownAdjacent = filter (\c -> getPlotCell old c == Unknown) [upC, downC, leftC, rightC]
-    new = old & mapL %~ (M.insert thisC Empty)
-              & queueL %~ (S.union (S.fromList unknownAdjacent))
-
-
-adjacentCells :: Point -> [Point]
-adjacentCells thisC =
-  let upC = thisC & _2 %~ pred
-      downC = thisC & _2 %~ succ
-      leftC = thisC & _1 %~ pred
-      rightC = thisC & _1 %~ succ
-  in [upC, downC, leftC, rightC]
 
 advance :: Direction -> Point -> Point
 advance North = over _2 pred
@@ -78,26 +60,110 @@ advance East = over _1 succ
 --           visit dir
 --       in undefined
 
+allDirections :: [Direction]
+allDirections = [North, South, West, East]
+
+dirToCommand :: Direction -> MachineWord
+dirToCommand North = 1
+dirToCommand South = 2
+dirToCommand West = 3
+dirToCommand East = 4
+
+inverse :: Direction -> Direction
+inverse South = North
+inverse North = South
+inverse West = East
+inverse East = West
+
 plot :: IO Map
 plot = do
-  let state = plotOnArrival (PlotState (0, 0) M.empty S.empty)
+  let origin = (0, 0)
+      initialStackOp dir = Push dir (advance dir origin)
+      state = PlotState (M.singleton origin Empty) (initialStackOp <$> allDirections)
+
   stateRef <- newIORef state
 
-  print state
+  let haltAction = liftIO $ putStrLn "halted"
 
+      inputAction :: App MachineWord = do
+        state <- liftIO $ readIORef stateRef
+        case state^.queueL of
+          [] -> do
+            haltedL .= True
+            pure 0
+          (Push dir pt:_) -> do
+            -- liftIO $ putStrLn $ "Advancing " ++ show dir ++ " to " ++ show pt
+            pure $ dirToCommand dir
+          (Pop dir:_) -> do
+            --liftIO $ putStrLn $ "Backtracking " ++ show dir
+            pure $ dirToCommand dir
 
-  let haltAction = undefined
-      inputAction = undefined
-      outputAction = undefined
+      outputAction result = do
+        state <- liftIO $ readIORef stateRef
+        case state^.queueL of
+          (Push dir pt:_) -> handlePushResult result dir pt
+          (Pop dir:_) -> handlePopResult result
+
+      handlePushResult result dir pt = do
+        state <- liftIO $ readIORef stateRef
+        case result of
+          0 -> liftIO $ do
+            -- putStrLn $ "Hit a wall at " ++ show pt
+            writeIORef stateRef (state & queueL %~ tail
+                                       & mapL %~ (M.insert pt Wall))
+          _ -> do
+            let cell :: Cell = toEnum $ fromIntegral result
+                rollback = Pop (inverse dir)
+                possibleDirections = filter (\d -> getPlotCell state (advance d pt) == Unknown) allDirections
+                pushes = (\d -> Push d (advance d pt)) <$> possibleDirections
+                queue' = pushes ++ [rollback] ++ (tail $ state^.queueL)
+                state' = state & queueL .~ queue'
+                               & mapL %~ (M.insert pt cell)
+            liftIO $ do
+              writeIORef stateRef state'
+
+      handlePopResult 0 = error "Failed to backtrack"
+      handlePopResult _ = liftIO $ do
+        state <- liftIO $ readIORef stateRef
+        writeIORef stateRef (state & queueL %~ tail)
+
 
   let opcodes = mkOpcodesTable (pureOpcodes ++ mkIoOpcodes haltAction inputAction outputAction)
   vm <- readProgram "15.txt" >>= flip makeRunnable opcodes
 
   runIntcode vm runVM
 
-  pure M.empty
+  finalState <- readIORef stateRef
+
+  pure $ finalState^.mapL
+
+
+blockSize :: Int
+blockSize = 4
+
+tileAt :: Int -> Int -> Picture -> Picture
+tileAt x y pic = Translate (fromIntegral $ x * blockSize) (negate $ fromIntegral $ y * blockSize) $ Scale (fromIntegral blockSize) (fromIntegral blockSize) pic
+
+square :: Picture
+square = Polygon [(0, 0), (0, 1), (1, 1), (1, 0)]
+
+cellToPicture :: Cell -> Picture
+cellToPicture Empty = Color black square
+cellToPicture Wall = Color red square
+cellToPicture OxygenTank = Color green square
+cellToPicture Unknown = Color blue square
+
+mapToPicture :: Map -> Picture
+mapToPicture m = Pictures $
+  (\((x, y), cell) -> tileAt x y (cellToPicture cell)) <$> M.toList m
+
+
+findTank :: Map -> Point
+findTank m = fst $ head $ filter (\(_, c) -> c == OxygenTank) (M.toList m)
 
 main :: IO ()
 main = do
   knownMap <- plot
+  -- display (InWindow "Nice Window" (200, 200) (10, 10)) red (mapToPicture knownMap)
+  print $ findTank knownMap
   pure ()
