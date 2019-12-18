87e42c54d3e25d7de8661c2a308012ab89e9d31a
Alexey Lebedeff <binarin@binarin.ru>
2019-12-18 21:31:02 +0100

    Day 18 - part 1 - WIP

--- 18.hs
+++ 18.hs
@@ -8,9 +8,13 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
+-- {-# LANGUAGE Strict #-}
 module Main where
 
-import Control.Monad (forM, when)
+import Data.List (sortOn)
+import Debug.Trace
+import GHC.Stack
+import Control.Monad (forM, when, foldM)
 import Data.Array.ST
 import Data.Array
 import Control.Lens hiding (Empty)
@@ -18,6 +22,7 @@ import Control.Lens.TH
 import qualified Data.Set as S
 import qualified Data.PSQueue as PSQ
 import Data.PSQueue (Binding((:->)))
+import Data.Char (toLower)
 
 data Cell = Empty | Wall | Entrance | Key Char | Door Char deriving (Eq, Show)
 
@@ -39,7 +44,7 @@ fromChar '#' = Wall
 fromChar '@' = Entrance
 fromChar c
   | c >= 'a' && c <= 'z' = Key c
-  | c >= 'A' && c <= 'Z' = Door c
+  | c >= 'A' && c <= 'Z' = Door (toLower c)
 fromChar c = error $ "Unparsable char " ++ [c]
 
 parseMap :: String -> Map
@@ -61,7 +66,7 @@ getCell' :: Point -> Map -> Cell
 getCell' = uncurry getCell
 
 findCell :: Map -> Cell -> Point
-findCell m c = (idx, idx)
+findCell m c = (x, y)
   where idx = fst $ head $ filter ((==c).view _2) (assocs (m^.cellsL))
         x = idx `rem` (m^.widthL)
         y = idx `div` (m^.widthL)
@@ -71,6 +76,22 @@ getCoord idx m = (idx `rem` (m^.widthL), idx `div` (m^.widthL))
 
 getIndex :: Point -> Map -> Int
 getIndex (x, y) m = (y * m^.widthL + x)
+
+reachableKeys :: Map -> KeySet -> Point -> [(Char, Point, Int)]
+reachableKeys m ks pt =
+  let wantedKey (idx, (cost, True)) =
+        case getCell' (getCoord idx m) m of
+          Key c -> not $ S.member c ks
+          _ -> False
+      wantedKey _ = False
+      extract (idx, (cost, _)) =
+        let pt = (getCoord idx m)
+            Key c = getCell' pt m
+        in (c, pt, cost)
+      costs = filter wantedKey (assocs (reachability m ks pt))
+
+  in extract <$> costs
+
 
 reachability :: Map -> KeySet -> Point -> Array Int (Int, Bool)
 reachability m ks pt = runSTArray $ do
@@ -104,48 +125,46 @@ reachability m ks pt = runSTArray $ do
                 adjacent = flip getIndex m <$> [(x, y-1), (x, y+1), (x-1, y), (x+1,y)]
                 reachable = filter isReachable adjacent
                 targetCost = cost + 1
-            forM reachable $ \i -> do
-              iCost <- getCost i
-              when
-              pure ()
-            pure ()
+                tryCell q idx = do
+                  thisCost <- getCost idx
+                  thisVisited <- isVisited idx
+                  if thisVisited || thisCost <= targetCost
+                    then pure q
+                    else pure $ PSQ.insert idx targetCost q
 
+            markVisited idx
+            setCost idx cost
+
+            foldM tryCell q' reachable >>= go
 
   go initialQueue
-
-
-  -- writeArray seen 0 False
-  -- writeArray cost 0 0
-  -- dijkstra (PSQ.singleton (getIndex pt m) 0)
   pure state
 
+allKeyMoves :: String -> Map -> KeySet -> Point -> Int -> Int
+allKeyMoves level m ks pt bound
+  | bound < 0 = 0
+  | otherwise =
+      let candidates = (\(_, _, cst) -> cst) `sortOn` reachableKeys m ks pt
+          descend (ch, pt, cost) bound =
+            let subCost = allKeyMoves (ch:level) m (S.insert ch ks) pt (bound - cost)
+            in -- ("Subcost", ch, reverse level, subCost) `traceShow`
+              cost + subCost
+          pickSmallest candidate minCost = min (descend candidate minCost) minCost
 
+      in if null candidates
+         then -- ("base", reverse level, ks, bound) `traceShow`
+           0
+         else -- (level, ks, bound) `traceShow`
+           foldr pickSmallest bound candidates
 
--- reachable :: Map -> KeySet -> Point -> [(Char, Coord, Int)]
--- reachable m ks coord = undefined
---   where
---     go st@(seen, found, xy, cost) =
---       case getCell' xy m of
---         Key c -> handleKey c st
---         Door c -> handleDoor c st
---         Wall -> st
---         Empty -> resumeSearh st
---         Entrance -> resumeSearh st
-
---     handleKey ch st@(seen, found, xy, cost) = undefined
---     handleDoor ch st@(seen, found, xy, cost) =
---       case S.member ch ks of
---         True ->
-
---     resumeSearh st@(seen, found, xy@(x, y), cost) =
---       let adjacent = [ (x, y-1), (x, y+1), (x-1, y), (x+1, y) ]
---           unseenAdj = filter (flip S.member seen) adjacent
---           seen' = S.insert xy seen
---       in foldr (\xy (seen, found, _, cost) -> go (seen, found, xy, cost + 1)) st unseenAdj
 
 main :: IO ()
 main = do
-  parsed <- parseMap <$> readFile "18-sample1.txt"
-  print parsed
+  parsed <- parseMap <$> readFile "18.txt"
+  -- print parsed
   let entrance = findCell parsed Entrance
+  putStrLn $ "Entrace at " ++ show entrance
+  print $ reachableKeys parsed S.empty entrance
+  -- print $ reachableKeys parsed (S.singleton 'a') (17, 1)
+  print $ allKeyMoves "" parsed S.empty entrance maxBound
   pure ()
