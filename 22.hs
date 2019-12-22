f01e6c86e62fa52fb43c09e93c5125a70d8456d6
Alexey Lebedeff <binarin@binarin.ru>
2019-12-22 21:06:55 +0100

    Day 22 - part 2

--- 22.hs
+++ 22.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE LambdaCase #-}
 module Main where
 
+import Control.Monad.Extra (whileM)
+import Data.STRef
+import Control.Loop
+import Data.IORef
 import Data.List
 import Control.Exception (throwIO, Exception)
 import Control.Monad.ST
@@ -21,13 +25,23 @@ opToLf mod DealNew = LF (-1) (-1) mod
 opToLf mod (Cut c) = LF 1 (-c) mod
 opToLf mod (DealInc i) = LF i 0 mod
 
+applyLf :: LF -> Integer -> Integer
+applyLf (LF a b m) x = (a * x + b) `mod` m
+
+expandLf :: LF -> [Integer]
+expandLf lf@(LF _ _ m) = runST $ do
+  v <- MV.new $ fromIntegral m
+  numLoop 0 (m-1) $ \x -> do
+    MV.write v (fromIntegral $ applyLf lf x) x
+  V.toList <$> V.freeze v
+
 intParser :: Parser Int
 intParser = do
   negative <- option id (negate <$ char '-')
   numbersString <- read <$> many1 digit
   pure $ negative numbersString
 
--- f1(f2(x))
+-- f1 `compose` f2 = f1(f2(x))
 compose :: LF -> LF -> LF
 compose (LF a1 b1 m) (LF a2 b2 _) =
   LF ((a1 * a2) `mod` m) ((a1 * b2 + b1) `mod` m) m
@@ -95,7 +109,7 @@ part1 = do
   let go [] d = pure d
       go (op:ops) d = do
         let d' = applyOp op d
-        -- print $ d'
+        print $ d'
         go ops d'
 
   ops <- parseFromFile shuffleFileParser "22.txt" >>= \case
@@ -118,6 +132,132 @@ part1 = do
   pure ()
 
 
+part1' :: IO ()
+part1' = do
+  ops <- parseFromFile shuffleFileParser "22.txt" >>= \case
+    Left error -> throwIO $ ParsingException $ show error
+    Right parsed -> pure parsed
+
+  let ct = 10007
+      startLf = LF 1 0 ct
+
+  let go _ [] d _ = pure d
+      go 0 _ d _ = pure d
+      go lim (op:ops) d lf = do
+        let d' = applyOp op d
+            oplf = opToLf ct op
+            lf' = oplf `compose` lf
+        print op
+        print $ (oplf, lf, lf')
+        print $ take 10 $ d'
+        print $ take 10 $ expandLf lf'
+        print $ [ applyLf (inverseLF lf') x | x <- [0..9]]
+        putStrLn ""
+        go (lim - 1) ops d' lf'
+
+  go 5 ops [0..ct-1] startLf
+  let lf = foldr (flip compose) (LF 1 0 ct) (opToLf ct <$> ops)
+  print lf
+  print $ applyLf lf 2019
+  pure ()
+
+lfPow :: LF -> Integer -> LF
+lfPow (LF a b m) 0 = LF 1 0 m
+lfPow lf         1 = lf
+lfPow lf         n =
+  let (halfN, rem) = n `divMod` 2
+      lf2 = lf `compose` lf
+  in case rem of
+    0 -> lfPow lf2 halfN
+    1 -> lf `compose` lfPow lf2 halfN
+
+
+
+part2 :: IO ()
+part2 = do
+  ops <- parseFromFile shuffleFileParser "22.txt" >>= \case
+    Left error -> throwIO $ ParsingException $ show error
+    Right parsed -> pure parsed
+
+  let deckSize = 119315717514047
+      iterations = 101741582076661
+      lf = foldr (flip compose) (LF 1 0 deckSize) (opToLf deckSize <$> ops)
+      lf' = lfPow lf iterations
+  print $ applyLf (inverseLF lf') 2020
+  pure ()
+
+inverse :: Integer -> Integer -> Integer
+inverse a n = runST $ do
+  t <- newSTRef 0
+  newT <- newSTRef 1
+  r <- newSTRef n
+  newR <- newSTRef a
+  whileM $ do
+    quot <- div <$> readSTRef r <*> readSTRef newR
+    (,) <$> readSTRef t <*> readSTRef newT >>= \(tV, newTV) -> do
+      writeSTRef t newTV
+      writeSTRef newT (tV - quot * newTV)
+    (,) <$> readSTRef r <*> readSTRef newR >>= \(rV, newRV) -> do
+      writeSTRef r newRV
+      writeSTRef newR (rV - quot * newRV)
+    (/= 0) <$> readSTRef newR
+  readSTRef t >>= \case
+    tv
+      | tv < 0 -> pure $ tv + n
+      | otherwise -> pure $ tv
+
+inverseLF :: LF -> LF
+inverseLF (LF a b m) =
+  let aInv = inverse a m
+  in LF aInv ((-b * aInv) `mod` m) m
+
+
 main :: IO ()
 main = do
+  -- part1'
+  -- print $ (3409 * (inverse 3409 10007)) `mod` 10007
+  part2
+
+  -- print $ LF 65 0 10007 `compose` LF (-1) (-1) 10007
+
+part1Tests :: IO ()
+part1Tests = do
+  ops <- parseFromFile shuffleFileParser "22.txt" >>= \case
+    Left error -> throwIO $ ParsingException $ show error
+    Right parsed -> pure parsed
+
+  let ct = 10
+
+  let go [] d = pure d
+      go (op:ops) d = do
+        let d' = applyOp op d
+        print $ d'
+        go ops d'
+
+  let prim1 = opToLf ct (Cut 6)
+      prim2 = opToLf ct (DealInc 7)
+      prim3 = opToLf ct (DealNew)
+
+      s1 = LF 1 0 ct `compose` prim1
+      s2 = s1 `compose` prim2
+      s3 = s2 `compose` prim3
+
+      ss = foldr compose (LF 1 0 ct) [prim1, prim2, prim3]
+
+  go [Cut 6, DealInc 7, DealNew] [0..ct - 1]
+
+  putStrLn ""
+  print $ expandLf prim1
+  print $ expandLf prim2
+  print $ expandLf prim3
+
+  putStrLn ""
+
+  print $ expandLf $ s1
+  print $ expandLf $ s2
+  print $ expandLf $ s3
+
+  putStrLn ""
+  print $ expandLf ss
+
   pure ()
